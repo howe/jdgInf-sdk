e45609bd463445b2c7e5ea986d668fa466b2ba30
Howe Chiang <howechiang@gmail.com>
2017-12-22 19:02:59 +0800

    add:获取订单信息

--- src/main/java/com/jiedangou/inf/sdk/util/inf/OrderUtil.java
+++ src/main/java/com/jiedangou/inf/sdk/util/inf/OrderUtil.java
@@ -129,7 +129,7 @@ public class OrderUtil {
             req.setVersion(Dict.JDG_API_VERSION);
             req.setBizData(Lang.obj2nutmap(biz));
             req.setSign(JdgUtil.getSign(Lang.obj2nutmap(req), key));
-            String json = HttpUtil.post(Dict.JDG_DEV_API_HOST + Dict.JDG_API_ACTION_ORDER_ACCEPTORDER, Json.toJson(req));
+            String json = HttpUtil.post(Dict.JDG_DEV_API_HOST + Dict.JDG_API_ACTION_ORDER_FETCHORDER, Json.toJson(req));
             if (Strings.isEmpty(json)) {
                 throw new Exception("返回值异常");
             } else {
